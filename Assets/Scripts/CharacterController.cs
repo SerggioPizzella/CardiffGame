0269f5240b21acfea9d676f199c15a79525612f1
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-04-26 15:18:07 +0100

    player

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,15 +5,25 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float speedMultiplier;
     private float _horizontal;
     private float _vertical;
+    private Vector3 _scale;
     private Rigidbody2D _rigidbody2D;
 
     void Start()
     {
+        _scale = transform.localScale;
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
         _horizontal = Input.GetAxis("Horizontal");
+        if (_horizontal < 0)
+        {
+            transform.localScale = new Vector3(_scale.x * -1, _scale.y, _scale.z);
+        }
+        else if (_horizontal > 0)
+        {
+            transform.localScale = new Vector3(_scale.x, _scale.y, _scale.z);
+        }
         _vertical = Input.GetAxis("Vertical");
     }
     
