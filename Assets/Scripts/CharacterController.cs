5839cc4d6c635b93046721cf64f56bae1a621125
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-04-28 10:57:55 +0100

    attack

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] private float speedMultiplier;
+    [SerializeField] private float speedMultiplier=7.5f;
+    [SerializeField] private float slowSpeedMultiplier=2.0f;
+    private float _currentSpeed;
     private float _horizontal;
     private float _vertical;
     private Vector3 _scale;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
 
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _scale = transform.localScale;
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+        if (_animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "AttackAnimation")
+        {
+            _currentSpeed = slowSpeedMultiplier;
+        }
+        else
+        {
+            _currentSpeed = speedMultiplier;
+        }
         _horizontal = Input.GetAxis("Horizontal");
         if (_horizontal < 0)
         {
@@ -25,11 +37,12 @@ public class CharacterController : MonoBehaviour
             transform.localScale = new Vector3(_scale.x, _scale.y, _scale.z);
         }
         _vertical = Input.GetAxis("Vertical");
+        _animator.SetBool("Attacking", Input.GetKey(KeyCode.Space));
     }
     
     void FixedUpdate()
     {
         Vector2 movement = new Vector2(_horizontal, _vertical);
-        _rigidbody2D.velocity = movement * speedMultiplier;
+        _rigidbody2D.velocity = movement * _currentSpeed;
     }
 }
