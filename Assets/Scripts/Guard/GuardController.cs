e3b8ede0e0e6281eeaf267bd967a5b74cb9ea7e3
Ignace Shoeib <ignace.shoeib@student.ap.be>
2022-04-28 22:49:24 +0100

    damage

--- Assets/Scripts/Guard/GuardController.cs
+++ Assets/Scripts/Guard/GuardController.cs
@@ -6,6 +6,27 @@ using UnityEngine;
 public class GuardController : MonoBehaviour
 {
     [SerializeField] private AIPath aiPath;
+    public float DamageCooldown=1;
+    private float dealtDamage;
+    private GameObject player;
+
+    public void Start()
+    {
+        player = FindObjectOfType<CharacterController>().gameObject;
+    }
+
+    public void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) <= 1)
+        {
+            if (dealtDamage >= DamageCooldown)
+            {
+                player.GetComponent<CharacterController>().Health-=25;
+                dealtDamage = 0;
+            }
+            dealtDamage += Time.deltaTime;
+        }
+    }
 
     public void startMoving()
     {
